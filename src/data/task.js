f59785e94eae68498db2f10394428d5da94d4789
Tiquillo <ldelgado@gmail.com>
2023-05-06 23:02:01 -0600

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Mi primer tarea de react",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Mi segunda tarea de react",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Mi tercer tarea de react",
+  },
+];
